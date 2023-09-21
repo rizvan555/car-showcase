ed3f3c45ce571a686d6516563ccffbad4e6d640d
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-09-21 15:18:12 +0200

    created Cardetails component

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': 'd85ed4a875mshc32a0b22657c31bp1e4420jsn3af5ff66d9d3',
@@ -5,7 +7,7 @@ export async function fetchCars() {
   };
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
     { headers: headers }
   );
   const result = response.json();
@@ -23,4 +25,22 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRentPerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRentPerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    'customer',
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ''
+  );
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
